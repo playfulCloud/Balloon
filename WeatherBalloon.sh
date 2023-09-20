dd458aae44ae8a16de0188d48a1b9391e5d19019
Jakub Ptaszkowski <55619673+playfulCloud@users.noreply.github.com>
2023-09-20 21:36:57 +0200

    Add files via upload

---
+++ WeatherBalloon.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+clear
+echo "    __          __               _     _                       ____            _   _                           "
+echo "    \ \        / /              | |   | |                     |  _ \          | | | |                          "
+echo "     \ \  /\  / /  ___    __ _  | |_  | |__     ___   _ __    | |_) |   __ _  | | | |   ___     ___    _ __    "
+echo "      \ \/  \/ /  / _ \  / _  | | __| | '_ \   / _ \ | '__|   |  _ <   / _  | | | | |  / _ \   / _ \  |  _ \   "
+echo "       \  /\  /  |  __/ | (_| | | |_  | | | | |  __/ | |      | |_) | | (_| | | | | | | (_) | | (_) | | | | |  "
+echo "        \/  \/    \___|  \__,_|  \__| |_| |_|  \___| |_|      |____/   \__,_| |_| |_|  \___/   \___/  |_| |_|  "
+echo " --------------------------------------------------------------------------------------------------------------- "
+echo "                       Welcome to playfulCloud's  Weather Balloon - Arch Linux Install script                    "
+echo " --------------------------------------------------------------------------------------------------------------- "
+echo ""
+echo ""
+   echo "   Please enter which type of Cloud are you? "
+   echo "   1 - NimboStratus - Desktop "
+   echo "   2 - CumuloNimbus - Remote " 
+read -p "   Enter correct number: " type
+loadkeys pl
+timedatectl set-ntp true
+if [ $type -eq 1 ] ; then
+   computerName = "NimboStratus"
+   echo " --------------------------------------------------------------------------------------------------------------- "
+   echo "                    __ _           _           __ _             _              "
+   echo "                 /\ \ (_)_ __ ___ | |__   ___ / _\ |_ _ __ __ _| |_ _   _ ___  "
+   echo "                /  \/ / |  _   _ \|  _ \ / _  \ \| __|  __/ _ | __| |  | / __| "
+   echo "               / /\  /| | | | | | | |_) | (_) |\ \ |_| | | (_| | |_| |_| \__ \ "
+   echo "               \_\ \/ |_|_| |_| |_|_.__/ \___/\__/\__|_|  \__,_|\__|\__,_|___/ "
+   #echo " --------------------------------------------------------------------------------------------------------------- "                                                              
+   ip addr show
+else 
+   computerName = "CumuloNimbus"
+echo " --------------------------------------------------------------------------------------------------------------- "   
+echo "                 ___                      _           __ _           _                "
+echo "                / __\   _ _ __ ___  _   _| | ___   /\ \ (_)_ __ ___ | |__  _   _ ___  "
+echo "               / / | | | |  _   _ \| | | | |/ _ \ /  \/ / |  _   _ \|  _ \| | | / __| "
+echo "              / /__| |_| | | | | | | |_| | | (_) / /\  /| | | | | | | |_) | |_| \__ \ "
+echo "              \____/\__,_|_| |_| |_|\__,_|_|\___/\_\ \/ |_|_| |_| |_|_.__/ \__,_|___/ "
+echo " --------------------------------------------------------------------------------------------------------------- "                                                                       
+   echo "   Now we will set up internet connection: "
+   iwctl
+   device list
+   read -p "   Enter the interface name you want to connect: " interface
+    station $interface scan
+    station $interface get-networks
+   read -p "   Enter the wireless network name you want to connect: " network
+    station $interface connect $network
+   echo "   CTRL + D -> to exit iwctl prompt"
+   echo " --------------------------------------------------------------------------------------------------------------- "
+   echo "   Check if the ip adress is aquired"
+    ip addr show
+   echo "   If ping went well your network is set"
+    ping -c 2 8.8.8.8
+    pacman -S mesa
+fi
+echo " --------------------------------------------------------------------------------------------------------------- "
+lsblk
+read -p "Enter the name of the EFI partition: " sda1
+read -p "Enter the name of the LVM partition: " sda2
+mkfs.fat -F32 /dev/$sda1 #okej 
+pvcreate --dataalignment 1m /dev/$sda2 #okej 
+vgcreate volgroup0 /dev/$sda2 #okej 
+
+lvcreate -L 30GB volgroup0 -n lv_root 
+lvcreate -l 100%FREE volgroup0 -n lv_home
+
+modprobe dm_mod 
+vgscan 
+vgchange -ay
+
+mkfs.ext4 /dev/volgroup0/lv_root
+mount /dev/volgroup0/lv_root /mnt 
+
+mkfs.ext4 /dev/volgroup0/lv_home
+mkdir /mnt/home
+mount /dev/volgroup0/lv_home /mnt/home
+mkdir /mnt/etc
+
+genfstab -U -p /mnt >> /mnt/etc/fstab
+echo " --------------------------------------------------------------------------------------------------------------- "
+echo "       ___ _              ___           _   _ _   _                      ___                     _  "
+echo "      /   (_)___  ___    / _ \__ _ _ __| |_(_) |_(_) ___  _ __  ___     /   \___  _ __   ___    / \ "
+echo "     / /\ / / __|/ __|  / /_)/ _  |  __| __| | __| |/ _ \|  _ \/ __|   / /\ / _ \|  _ \ / _ \  /  / "
+echo "    / /_//| \__ \ (__  / ___/ (_| | |  | |_| | |_| | (_) | | | \__ \  / /_// (_) | | | |  __/ /\_/  "
+echo "   /___,' |_|___/\___| \/    \__,_|_|   \__|_|\__|_|\___/|_| |_|___/ /___,' \___/|_| |_|\___| \/    "
+echo " --------------------------------------------------------------------------------------------------------------- "
+
+pacstrap -i /mnt base 
+arch-chroot /mnt ./Balloon/WeatherBalloon2.sh
+
+
+
+
+
+
+
+
+                                                                                               
+
+
+
+
+
+
+
---
+++ WeatherBalloon2.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+echo " __    __           _   _                   ___       _ _                       ___             __ _         ____  "
+echo "/ / /\ \ \___  __ _| |_| |__   ___ _ __    / __\ __ _| | | ___   ___  _ __     / __\___  _ __  / _(_) __ _  |___ \ "
+echo "\ \/  \/ / _ \/ _  | __| '_ \ / _ \ '__|  /__\/// _  | | |/ _ \ / _ \| '_ \   / /  / _ \| '_ \| |_| |/ _  |   __) | "
+echo " \  /\  /  __/ (_| | |_| | | |  __/ |    / \/  \ (_| | | | (_) | (_) | | | | / /__| (_) | | | |  _| | (_| |  / __/  "
+echo "  \/  \/ \___|\__,_|\__|_| |_|\___|_|    \_____/\__,_|_|_|\___/ \___/|_| |_| \____/\___/|_| |_|_| |_|\__, | |_____| "
+echo "                                                                                                      |___/         "
+echo " --------------------------------------------------------------------------------------------------------------- "
+echo "                      playfulCloud's  Weather Balloon - Arch Linux Install script part2                    "
+echo " --------------------------------------------------------------------------------------------------------------- "
+pacman -S linux-lts linux-lts-headers 
+pacman -S base-devel openssh
+systemctl enable sshd
+pacman -S networkmanager wpa_supplicant wireless_tools netctl
+pacman -S dialog
+systemctl enable NetworkManager
+pacman -S lvm2
+
+echo "Add LVM suppor - > HOOKS(block "there - lvm2" filesystems) "
+read -p "Open it now?" c
+vim /etc/mkinitcpio.conf
+mkinitcpio -p linux-lts 
+
+
+keyboardlayout="pl"
+zoneinfo="Europe/Warsaw"
+
+echo "pl_PL.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+
+ln -sf /usr/share/zoneinfo/$zoneinfo /etc/localtime
+hwclock --systohc
+
+
+
+echo "FONT=ter-v18n" >> /etc/vconsole.conf
+echo "KEYMAP=$keyboardlayout" >> /etc/vconsole.conf
+echo "$computerName" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 $computerName.localdomain $computerName" >> /etc/hosts
+
+
+echo "  Set new root password! "
+passwd
+
+echo "  Adding playfulCloud as a new user:  "
+useradd -m -G wheel playfulCloud
+echo " Set the playfulClouds password: "
+passwd
+
+
+echo "Uncomment %wheel group in sudoers (around line 85):"
+read -p "Open it now?" c
+
+EDITOR=vim sudo -E visudo
+
+
+echo " --------------------------------------------------------------------------------------------------------------- "
+echo "      ___           _                         __ _                       _   _                   _                    _  "
+echo "     / __\ __ _ ___(_) ___    ___ ___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __     __| | ___  _ __   ___  / \ "
+echo "    /__\/// _  / __| |/ __|  / __/ _ \|  _ \| |_| |/ _  | | | |  __/ _  | __| |/ _ \|  _ \   / _  |/ _ \|  _ \ / _ \/  / "
+echo "   / \/  \ (_| \__ \ | (__  | (_| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | | | (_| | (_) | | | |  __/\_/  "
+echo "   \_____/\__,_|___/_|\___|  \___\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|  \__,_|\___/|_| |_|\___\/    "
+echo "                                                   |___/                                                                 "
+echo " --------------------------------------------------------------------------------------------------------------- "
+
+pacman -S grub efibootmgr dosfstools os-prober mtools 
+mkdir /boot/EFI 
+lsblk 
+read -p "Mount efi partition " partycja
+mount /dev/$partycja /boot/EFI 
+
+grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck 
+cp /usr/share/locale/en\@quot/LS_MESSAGES/grub.mo /boot/grub/locale/en.mo
+
+echo " --------------------------------------------------------------------------------------------------------------- "
+echo "   ___            _                        __ _                       _   _                   _                    _ "
+echo "  / _ \_ __ _   _| |__     ___ ___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __     __| | ___  _ __   ___  / \ "
+echo " / /_\/ '__| | | | '_ \   / __/ _ \| '_ \| |_| |/ _  | | | |  __/ _  | __| |/ _ \|  _ \   / _  |/ _ \| '_ \ / _ \/  / "
+echo "/ /_\ | |  | |_| | |_) | | (_| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | | | (_| | (_) | | | |  __/\_/  "
+echo "\____/|_|   \__,_|_.__/   \___\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|  \__,_|\___/|_| |_|\___\/   "
+echo "                                                |___/                                                                "
+echo " --------------------------------------------------------------------------------------------------------------- "
+
+pacman -S nvidia linux-lts  nvidia-lts 
